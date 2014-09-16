95a751b16e09a1b237d7cdc44ffb58a906bc531b
Christopher Armstrong <radix@twistedmatrix.com>
2014-09-16 11:45:28 -0500

    much better everything

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,6 @@
 import sys
 
+from functools import partial
 import six
 from six.moves import range
 
@@ -23,58 +24,74 @@ from pyrsistent import v, s
 # PYRSISTENT:
 # - no insert, but they do have assoc.
 
+# library
+MUTABLE = 'built-in mutable'
+PYRSISTENT = 'pyrsistent'
+PYSISTENCE = 'pysistence'
 
-# Appending
-def pysistence_vectors_append(size):
-    l = make_list(0)
-    for i in range(size):
-        l = l.concat(make_list(i))
+# operations
+VECTOR_APPEND = 'vector append'
+VECTOR_PUSH = 'vector push'
+VECTOR_MUTATE_BEG = 'vector mutate beginning'
+VECTOR_MUTATE_MID = 'vector mutate middle'
+VECTOR_MUTATE_END = 'vector mutate end'
 
-def pyrsistent_vectors_append(size):
-    l = v(0)
-    for i in range(size):
-        l = l.append(i)
+def benchmarks(size):
+    pys_list = make_list(*range(size))
+    pyr_list = v(*range(size))
+    mut_list = list(range(size))
 
-def mutable_vectors_append(size):
-    l = [0]
-    for i in range(size):
-        l.append(i)
+    pys_vector_append = partial(pys_list.concat, make_list(1))
+    pyr_vector_append = partial(pyr_list.append, 1)
+    mut_vector_append = partial(mut_list.append, 1)
 
+    mut_list = list(range(size))
 
-# Pushing
-def pysistence_vectors_push(size):
-    l = make_list([0])
-    for i in range(size):
-        l = l.cons(i)
+    pys_vector_push = partial(pys_list.cons, 1)
+    pyr_vector_push = partial(v(1).extend, pyr_list)
+    mut_vector_push = partial(mut_list.insert, 0, 1)
 
-def pyrsistent_vectors_push(size):
-    l = v(0)
-    for i in range(size):
-        l = v(i).extend(l)
+    mut_list = list(range(size))
 
-def mutable_vectors_push(size):
-    l = [0]
-    for i in range(size):
-        l.insert(0, i)
+    pys_vector_mutate_beginning = partial(_pys_set, 0, 'new-value', pys_list)
+    pyr_vector_mutate_beginning = partial(pyr_list.assoc, 0, 'new-value')
+    mut_vector_mutate_beginning = partial(mut_list.__setitem__, 0, 'new-value')
 
+    mut_list = list(range(size))
 
-# modifying existing items
+    middle_index = size // 2
+    pys_vector_mutate_middle = partial(_pys_set, middle_index, 'new-value', pys_list)
+    pyr_vector_mutate_middle = partial(pyr_list.assoc, middle_index, 'new-value')
+    mut_vector_mutate_middle = partial(mut_list.__setitem__, middle_index, 'new-value')
 
-def pysistence_vectors_mutate(size):
-    l = make_list(*range(size))
-    for i in range(size):
-        l = _pys_set(i, 'new-value', l)
+    mut_list = list(range(size))
 
-def pyrsistent_vectors_mutate(size):
-    l = v(*range(size))
-    for i in range(size):
-        l = l.assoc(i, 'new-value')
+    pys_vector_mutate_end = partial(_pys_set, size-1, 'new-value', pys_list)
+    pyr_vector_mutate_end = partial(pyr_list.assoc, size-1, 'new-value')
+    mut_vector_mutate_end = partial(mut_list.__setitem__, size-1, 'new-value')
 
+    benchmarks = [
+        (MUTABLE, VECTOR_APPEND, mut_vector_append),
+        (PYRSISTENT, VECTOR_APPEND, pyr_vector_append),
+        (PYSISTENCE, VECTOR_APPEND, pys_vector_append),
 
-def mutable_vectors_mutate(size):
-    l = list(range(size)) # copy the list just to be a little bit fair to how we construct the lists in other tests.
-    for i in range(size):
-        l[i] = 'new-value'
+        (MUTABLE, VECTOR_PUSH, mut_vector_push),
+        (PYRSISTENT, VECTOR_PUSH, pyr_vector_push),
+        (PYSISTENCE, VECTOR_PUSH, pys_vector_push),
+
+        (MUTABLE, VECTOR_MUTATE_BEG, mut_vector_mutate_beginning),
+        (PYRSISTENT, VECTOR_MUTATE_BEG, pyr_vector_mutate_beginning),
+        (PYSISTENCE, VECTOR_MUTATE_BEG, pys_vector_mutate_beginning),
+
+        (MUTABLE, VECTOR_MUTATE_MID, mut_vector_mutate_middle),
+        (PYRSISTENT, VECTOR_MUTATE_MID, pyr_vector_mutate_middle),
+        (PYSISTENCE, VECTOR_MUTATE_MID, pys_vector_mutate_middle),
+
+        (MUTABLE, VECTOR_MUTATE_END, mut_vector_mutate_end),
+        (PYRSISTENT, VECTOR_MUTATE_END, pyr_vector_mutate_end),
+        (PYSISTENCE, VECTOR_MUTATE_END, pys_vector_mutate_end),
+    ]
+    return benchmarks
 
 
 # Utilities
@@ -113,49 +130,18 @@ def _pys_set(i, newv, l):
 
 # Benchmark
 
-def repeat(f, vector_size):
-    results = timeit.repeat(lambda: f(vector_size), number=1000, repeat=3)
-    return sum(results) / len(results)
-
-
-# library
-MUTABLE = 'built-in mutable'
-PYRSISTENT = 'pyrsistent'
-PYSISTENCE = 'pysistence'
-
-# operations
-VECTOR_APPEND = 'vector append'
-VECTOR_PUSH = 'vector push'
-VECTOR_MUTATE = 'vector mutate'
-
-benchmarks = [
-    (MUTABLE, VECTOR_APPEND, mutable_vectors_append),
-    (PYRSISTENT, VECTOR_APPEND, pyrsistent_vectors_append),
-    (PYSISTENCE, VECTOR_APPEND, pysistence_vectors_append),
-
-    (MUTABLE, VECTOR_PUSH, mutable_vectors_push),
-    (PYRSISTENT, VECTOR_PUSH, pyrsistent_vectors_push),
-    (PYSISTENCE, VECTOR_PUSH, pysistence_vectors_push),
-
-    (MUTABLE, VECTOR_MUTATE, mutable_vectors_mutate),
-    (PYRSISTENT, VECTOR_MUTATE, pyrsistent_vectors_mutate),
-    (PYSISTENCE, VECTOR_MUTATE, pysistence_vectors_mutate),
-]
-
-
 for size in sys.argv[1:]:
     size = int(size)
-    for lib, op, func in benchmarks:
-        print("{} {} (size {}): {}".format(lib, op, size, repeat(func, size)))
-
-
-# TODO:
-
-# - the accumulation functions are a bit crappy, it'd probably be better to
-#   just do one operation per vector of a particular size, and then graph
-#   the performance of that operation over various sizes.
-#   but maybe accumulation speed is also useful? JIT/cache performance may
-#   differ.
+    print "==== data structure benchmarks ===="
+    print
+    print "initial structure size is {}".format(size)
+    print "times are for 10k ops"
+    print "mutable versions are left to grow between operations, so timings may be skewed."
+    print
+    for lib, op, func in benchmarks(size):
+        result = timeit.timeit(func, number=10000)
+        milliseconds = result * 1000
+        print("{:<20} {:<25} {:>22.2f} milliseconds".format(lib, op, milliseconds))
 
 
 # ANALYSIS OF RESULTS:
@@ -175,6 +161,21 @@ for size in sys.argv[1:]:
 
 
 # PYRSISTENT
-# - for some reason pushing to the front is WAY, WAY slower than appending.
-#   probably worth filing a bug.
+# - pushing is much slower than appending, because it's really an append of
+#   all elements onto a new list of length one.
+# - mutating is pretty fast and constant across the board.
+# - there is no insertion operation, so there's a ton of reallocation if you
+#   want to put something at the beginning or middle. (this is same as the
+#   issue with push)
 
+## SUMMARY
+
+# - of course, mutable vectors are pretty dang fast across the board, but the
+#   one place where some persistent data structures (in this case, only
+#   pysistence) wins is pushes onto the front.
+# - However, if you're MOSTLY pushing onto the front, you're probably better
+#   off just appending onto a different data structure (both mutable and
+#   pyrsistent are faster at appending than pysistence is at pushing).
+
+# - Apparently RRB-Trees and finger-trees are both better than all of these
+#   things at insertion.
